0432304538e9fbc1ceb9fdb88012b4b945d4a08c
zbeisler <zbeisler@secondpillar.com>
2022-01-13 01:46:07 -0500

    (update) prettified and added QOL features

--- __main__.py
+++ __main__.py
@@ -1,13 +1,32 @@
+import os
 import Controller as controller
 
+def print_help():
+    print()
+    with open("help.txt") as help_file:
+        for line in help_file:
+            print(line, end="")
+    print()
+    input("Press any key to continue...")
+    print()
+
 def main():
+    os.system('cls')
+    print("Welcome to WordleSolver. Guess \"help\" to display a help menu.")
     ls = load_words()
     while len(ls) > 1:
         guess = input(f"Enter guess (reccomended '{controller.choose_guess(ls)}'): ")
+        if guess == "*":
+            print(ls)
+            continue
+        elif guess == "help":
+            print_help()
+            continue
+        elif guess == "restart":
+            return main()
         pattern = input(f"Enter pattern: ")
         ls = controller.read_pattern(pattern, guess, ls)
     input(f"Congratulations, the word is {ls[0]}! Press any key to close...")
-
 
 def load_words():
     ls = []
